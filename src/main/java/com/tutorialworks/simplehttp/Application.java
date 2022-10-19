41ac6dbfd5411a6db9e4df8ba60037b8e514190a
sumpf <sumpf@hanmail.net>
2022-10-19 14:33:10 +0900

    Add files via upload

---
+++ src/main/java/com/tutorialworks/simplehttp/Application.java
@@ -0,0 +1,35 @@
+package com.tutorialworks.simplehttp;
+
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
+/**
+ * Hello world!
+ * Returns a simple web page on port 8080.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                // Set up the listener - you can change the port/host here
+                // 0.0.0.0 means "listen on ALL available addresses"
+                .addHttpListener(8080, "0.0.0.0")
+
+                .setHandler(exchange -> {
+                    // Sets the return Content-Type to text/html
+                    exchange.getResponseHeaders()
+                            .put(Headers.CONTENT_TYPE, "text/html");
+
+                    // Returns a hard-coded HTML document
+                    exchange.getResponseSender()
+                            .send("<html>" +
+                                    "<body>" +
+                                    "<h1>Hello, world!</h1>" +
+                                    "</body>" +
+                                    "</html>");
+                }).build();
+
+        // Boot the web server
+        server.start();
+    }
+}
